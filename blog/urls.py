52760de98222c16e485ba04ab8244d201a518c8f
vanguard88728 <vanguard88728@gmail.com>
2018-11-11 10:16:43 -0800

    Added View and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', view.index, name='index')
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,13 @@
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+from .models import Post
+
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
